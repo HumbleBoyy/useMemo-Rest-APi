2597832529ff1b5c4747d6475cc8b96fa08ac4d4
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-02-20 19:16:33 +0500

    new Commit

--- src/Pages/SinglePage.jsx
+++ src/Pages/SinglePage.jsx
@@ -29,13 +29,33 @@ export default function SinglePage() {
         </CardActionArea>
         <CardActionArea>
           <CardContent className='h-[500px] p-2'>
-            <Typography gutterBottom variant="h5" component="div" className=''>
+            <Typography gutterBottom variant="h5" component="div" className='!text-[55px]'>
               {singleProduct.title}
             </Typography>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            <Typography gutterBottom variant="h5" component="div">
+              <span  className='text-black font-bold'>Rating: </span>
+              {singleProduct.rating}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              <span  className='text-black font-bold'>Category: </span>
+              {singleProduct.category}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div" className='text-green-500'>
+              <span  className='text-black font-bold'>Brand: </span>
+              {singleProduct.brand}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div" className='text-red-500'>
+              <span  className='text-black font-bold'>Return: </span>
+              {singleProduct.returnPolicy}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div" className='text-blue-600'>
+              <span  className='text-black font-bold'>Shipping: </span>
+              {singleProduct.shippingInformation}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }} className='!text-[18px]'>
                 {singleProduct.description}
             </Typography>
-            <div className='flex justify-self-auto'>
+            <div className='flex justify-self-auto mt-10'>
               <Button onClick={()=> navigate(-1)} size='large' variant="outlined" className='w-full' startIcon={<ArrowBack/>}>Back</Button>
             </div>
           </CardContent>
